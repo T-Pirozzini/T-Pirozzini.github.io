b4ae30879830ee5b6570492fd0e6e7b8f8132b81
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-07-18 16:23:55 -0700

    setup project card styles/positioning

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Projects from './components/Projects.jsx'
-import logo from './assets/triforce.png'
+import logo from './assets/images/triforce.png'
 
 function App() {
   return (
     <div className="App">
-      <a href="index.html" class="logo">
-				<span class="symbol"><img src={logo} alt="triforce" /></span><span class="title">Travis Pirozzini</span>
+      <a href="index.html" className="logo">
+				<span className="symbol"><img src={logo} alt="triforce" /></span>
 			</a>
       <h1>Welcome to my Portfolio!</h1>
 			<p>Straight to the action - hover + click to access live demos below...</p>
