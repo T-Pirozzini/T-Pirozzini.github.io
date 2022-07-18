f315fcfe1d241f59a18e4074353f8b68b9454916
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-07-18 16:49:02 -0700

    add all projects and deployment links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,6 +5,17 @@ import './Projects.css'
 
 // project images
 import Spuddy from '../assets/images/spuddies-home.png'
+import FoodTruck from '../assets/images/food-truck-home.png'
+import GPT from '../assets/images/GPT-home.png'
+import Finance from '../assets/images/finance-tracker-home.png'
+import RPS from '../assets/images/rps-home.png'
+import Book from '../assets/images/bookshelf-home.png'
+import Calculator from '../assets/images/calculator-home.png'
+import Recipe from '../assets/images/cooking-home.png'
+import Dnd from '../assets/images/dnd-home.png'
+import Etch from '../assets/images/etch-home.png'
+import Restaurant from '../assets/images/restaurant-home.png'
+import Ttt from '../assets/images/tictactoe-home.png'
 
 export default function Projects() {
   
@@ -16,42 +27,35 @@ export default function Projects() {
           <h2>Spuddies Restaurant</h2>
           <div>											
             <p>Food ordering application</p>
-            <p>Stack: HTML CSS JS PostgreSQL</p>
-            <p>API: Twilio</p>
+            <p>Stack: HTML CSS JS PostgreSQL Twilio</p>            
           </div>
         </a>        
       </article>
-      
-      <article>
-        <span className="image">
-          {/* <img src="images/food-truck-home.png" style="height: 280px;" alt="food truck application" /> */}
-        </span>
+
+      <article className="project">        
+        <img src={FoodTruck} alt="food truck application" />       
         <a href="https://628686b4787a3b03908a9839--chipper-buttercream-16e62d.netlify.app/">
           <h2>FoodTruck Finder</h2>
           <div className="content">
             <p>Food truck locating application</p>
-            <p>Stack: HTML CSS JS React PostgreSQL</p>
-            <p>API: Leaflet Mapbox Stripe</p>
+            <p>Stack: HTML CSS JS React PostgreSQL Leaflet Mapbox Stripe</p>
           </div>
-        </a>
+        </a>        
       </article>
-      <article className="style3">
-        <span className="image">
-          {/* <img src="images/GPT-home.png" style="height: 280px;" alt="GPT-3 application" /> */}
-        </span>
+
+      <article className="project">        
+        <img src={GPT} alt="open AI application" />       
         <a href="https://62857e96c2d87717e882e101--earnest-swan-080405.netlify.app/">
           <h2>Fun With GPT-3</h2>
           <div className="content">
             <p>OpenAI communication application</p>
-            <p>Stack: HTML CSS JS React</p>
-            <p>API: GPT-3 OpenAI</p>
+            <p>Stack: HTML CSS JS React GTP-3/OpenAI</p>            
           </div>
         </a>
       </article>
-      <article className="style4">
-        <span className="image">
-          {/* <img src="images//finance-tracker-home.png" style="height: 280px;" alt="Finance Tracking application" /> */}
-        </span>
+
+      <article className="project">        
+        <img src={Finance} alt="fianance tracker application" />       
         <a href="https://financetracker-f047d.web.app/">
           <h2>FinanceTracker</h2>
           <div className="content">
@@ -60,10 +64,9 @@ export default function Projects() {
           </div>
         </a>
       </article>
-      <article className="style5">
-        <span className="image">
-          {/* <img src="images/rps-home.png" style="height: 280px;" alt="Rock Paper Scissors game" /> */}
-        </span>
+
+      <article className="project">        
+        <img src={RPS} alt="rock, paper, scissors application" />       
         <a href="https://t-pirozzini.github.io/Rock-Paper-Scissors/">
           <h2>Rock/Paper/Scissors</h2>
           <div className="content">
@@ -71,10 +74,9 @@ export default function Projects() {
           </div>
         </a>
       </article>
-      <article className="style6">
-        <span className="image">
-          {/* <img src="images/etch-home.png" style="height: 280px;" alt="Etch-n-sketch application" /> */}
-        </span>
+
+      <article className="project">        
+        <img src={Etch} alt="etch n sketch application" />       
         <a href="https://t-pirozzini.github.io/Etch-N-Sketch/">
           <h2>Etch-N-Sketch</h2>
           <div className="content">
@@ -83,10 +85,9 @@ export default function Projects() {
           </div>
         </a>
       </article>
-      <article className="style2">
-        <span className="image">
-          {/* <img src="images/calculator-home.png" style="height: 280px;" alt="" /> */}
-        </span>
+
+      <article className="project">        
+        <img src={Calculator} alt="calculator application" />       
         <a href="https://t-pirozzini.github.io/Calculator/">
           <h2>Calculator</h2>
           <div className="content">
@@ -95,10 +96,9 @@ export default function Projects() {
           </div>
         </a>
       </article>
-      <article className="style3">
-        <span className="image">
-          {/* <img src="images/dnd-home.png" style="height: 280px;" alt="" /> */}
-        </span>
+
+      <article className="project">        
+        <img src={Dnd} alt="dnd application" />       
         <a href="https://t-pirozzini.github.io/landing_page/">
           <h2>D&D Landing Page</h2>
           <div className="content">
@@ -106,10 +106,9 @@ export default function Projects() {
           </div>
         </a>
       </article>
-      <article className="style1">
-        <span className="image">
-          {/* <img src="images/cooking-home.png" style="height: 280px;" alt="" /> */}
-        </span>
+
+      <article className="project">        
+        <img src={Recipe} alt="recipe application" />       
         <a href="https://cooking-web-application.firebaseapp.com/">
           <h2>Recipe Book</h2>
           <div className="content">
@@ -118,10 +117,9 @@ export default function Projects() {
           </div>
         </a>
       </article>
-      <article className="style5">
-        <span className="image">
-          {/* <img src="images/restaurant-home.png" style="height: 280px;" alt="" /> */}
-        </span>
+
+      <article className="project">        
+        <img src={Restaurant} alt="restaurant application" />       
         <a href="https://t-pirozzini.github.io/Restaurant-Page/">
           <h2>Restaurant Page</h2>
           <div className="content">
@@ -130,10 +128,9 @@ export default function Projects() {
           </div>
         </a>
       </article>
-      <article className="style6">
-        <span className="image">
-          {/* <img src="images/tictactoe-home.png" style="height: 280px;" alt="" /> */}
-        </span>
+
+      <article className="project">        
+        <img src={Ttt} alt="tic-tac-toe application" />       
         <a href="https://t-pirozzini.github.io/Tic-Tac-Toe/">
           <h2>Tic-Tac-Toe</h2>
           <div className="content">
@@ -142,10 +139,9 @@ export default function Projects() {
           </div>
         </a>
       </article>
-      <article className="style4">
-        <span className="image">
-          {/* <img src="images//bookshelf-home.png" style="height: 280px;" alt="" /> */}
-        </span>
+
+      <article className="project">        
+        <img src={Book} alt="book shelf application" />       
         <a href="https://t-pirozzini.github.io/Book-Shelf/">
           <h2>Book Shelf</h2>
           <div className="content">
@@ -153,7 +149,7 @@ export default function Projects() {
             <p>Stack: HTML CSS JS</p>
           </div>
         </a>
-      </article>      
+      </article>         
     </div>
   )
 }
