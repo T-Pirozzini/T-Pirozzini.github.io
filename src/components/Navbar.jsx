fb4e9021de84b4a6540d54d73b45801eb4665d4b
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-07-21 17:03:17 -0700

    add links to social icons - complete styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,13 +28,11 @@ export default function Navbar() {
         <p>Whether it is planting half a million trees, trail-racing on mountains, taking down an Ancient Blue Dragon in D&D, or learning to code - I seek the challenge.</p>
         <p>I am currently seeking my first full-time development role. Feel free to connect with me on your preferred social platform.</p>
         <div className="social-icons">
-          <span><VscGithub /></span>
-          <span><AiOutlineLinkedin /></span>
-          <span><SiStrava /></span>
-          <span><SiDiscord /></span>
-          <span><SiSlack /></span>
-          <span><SiSteam /></span>
-          <span><SiMinutemailer /></span>
+          <a href='https://github.com/T-Pirozzini'><VscGithub /></a>
+          <a href='https://www.linkedin.com/in/travis-pirozzini-2522b5115/'><AiOutlineLinkedin /></a>
+          <a href='https://www.strava.com/athletes/27155298'><SiStrava /></a>
+          <a href='discordapp.com/users/SlavTheTrayer#0083'><SiDiscord /></a>
+          <a href='mailto:tpirozzini@gmail.com'><SiMinutemailer /></a>
         </div>
       </div>
     </div>
